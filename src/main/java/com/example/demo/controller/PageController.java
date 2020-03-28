f7e286fdf418d34c38fa9014db5fdc5e86cadede
TONOT <toninhopalhas@terra.com.br>
2020-03-27 22:13:51 -0300

    demo1 teste

---
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -0,0 +1,13 @@
+//package com.example.demo.controller;
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class PageController {
+//
+//    @RequestMapping("/")
+//    public String home(){
+//        return "hi motherfuckers"
+//    }
+//}
